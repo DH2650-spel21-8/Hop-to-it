3103b28e6e08c8943b9a98ca5d2227fb4c060802
ypaetzelt <y.j.paetzelt@gmail.com>
2021-04-12 17:58:01 +0200

    Switch over to new Input System

--- Assets/Scripts/PlayerMovementV1.cs
+++ Assets/Scripts/PlayerMovementV1.cs
@@ -1,31 +1,38 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementV1 : MonoBehaviour
 {
     public float movementSpeed = 10;
     public float turningSpeed = 60;
 
+    private InputAction movementAction; 
+
     private Rigidbody _rb;
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
+
+        var map = new InputActionMap("Simple Player Controller");
+
+        movementAction = map.AddAction("move", binding: "<Gamepad>/leftStick");
+        movementAction.AddCompositeBinding("Dpad")
+            .With("Up", "<Keyboard>/w")
+            .With("Up", "<Keyboard>/upArrow")
+            .With("Down", "<Keyboard>/s")
+            .With("Down", "<Keyboard>/downArrow")
+            .With("Left", "<Keyboard>/a")
+            .With("Left", "<Keyboard>/leftArrow")
+            .With("Right", "<Keyboard>/d")
+            .With("Right", "<Keyboard>/rightArrow");
+        
+        movementAction.Enable();
     }
 
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        _rb.AddForce(new Vector3(horizontal, 0, 0));
-        //transform.Translate(horizontal, 0, 0);
-
-        float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        
-        _rb.AddForce(new Vector3(0, 0, vertical));
-        //transform.Translate(0, 0, vertical);
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        var hits = other.contacts;
+        var moveDelta = movementAction.ReadValue<Vector2>() * movementSpeed * Time.deltaTime;
+        _rb.AddForce(new Vector3(moveDelta.x, 0, moveDelta.y));
     }
 }
