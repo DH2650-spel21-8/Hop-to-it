2206afcbc3b701a50a28f2c90f014fc1c7d4b427
marchamil <74551298+marchamil@users.noreply.github.com>
2021-05-24 11:33:35 +0200

    Added edible food and lightswitches
    New prefabs for food items make them edible by playing the sound and destroying the object.
    Lightswitches are interactable with sound. No other function atm.

--- Assets/Scripts/PlayAudio.cs
+++ Assets/Scripts/PlayAudio.cs
@@ -13,6 +13,14 @@ public class PlayAudio : MonoBehaviour
         audiosource = GetComponent<AudioSource>();
     }
 
+    // Let the audio finish before destroying the object.
+    public void PlayAndDestroy()
+    {
+        GetComponent<Renderer>().enabled = false;
+        PlaySound();
+        Destroy(this, 0.5f);
+    }
+
     public void PlaySound()
     {
         // Yeah I know it's bad...
